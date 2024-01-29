982ada52312a15cb8cfbfe2b88d8b37f629f636c
Hossein <barari.h84@gmail.com>
2024-01-29 22:52:35 +0330

    new dives

---
+++ okkkk digi bal/script.js
@@ -0,0 +1,57 @@
+$('#MenuBox').mouseenter(function () {
+    $('#Load').fadeIn(250);
+
+})
+$('#Load').mouseleave(function () {
+    $('#Load').fadeOut(250);
+
+})
+
+$('#MenuBox1').mouseenter(function () {
+    $('#Load').fadeIn(250);
+
+})
+$('#Load').mouseleave(function () {
+    $('#Load').fadeOut(250);
+
+})
+$('#MenuBox2').mouseenter(function () {
+    $('#Load').fadeIn(250);
+
+})
+$('#Load').mouseleave(function () {
+    $('#Load').fadeOut(250);
+
+})
+$('#MenuBox3').mouseenter(function () {
+    $('#Load').fadeIn(250);
+
+})
+$('#Load').mouseleave(function () {
+    $('#Load').fadeOut(250);
+
+})
+$('#MenuBox4').mouseenter(function () {
+    $('#Load').fadeIn(250);
+
+})
+$('#Load').mouseleave(function () {
+    $('#Load').fadeOut(250);
+
+})
+$('#MenuBox5').mouseenter(function () {
+    $('#Load').fadeIn(250);
+
+})
+$('#Load').mouseleave(function () {
+    $('#Load').fadeOut(250);
+
+})
+$('#MenuBox6').mouseenter(function () {
+    $('#Load').fadeIn(250);
+
+})
+$('#Load').mouseleave(function () {
+    $('#Load').fadeOut(250);
+
+})
